9f0bd7b65c6d8e793994277350d509b2c009736f
Aida Eslami <ayda.eslami@gmail.com>
2026-06-26 20:55:53 +0100

    Primitive data type

---
+++ src/Exe2/PrimitiveTypes.java
@@ -0,0 +1,21 @@
+
+package Exe2;
+
+// public: Accessible from anywhere in the program.
+public class PrimitiveTypes {
+    // static: Belongs to the class, not an object.
+    // void: This method does not return a value.
+    public static void main(String[] args) {
+        int iNum=10;
+        double dNumber=10.5;
+        boolean bValue=true;
+        char cChar='A';
+
+        System.out.println(iNum+" "+dNumber+" "+bValue+" "+cChar );
+
+
+    }
+}
+
+
+
